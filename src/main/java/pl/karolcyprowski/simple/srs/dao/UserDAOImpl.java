b91071683c56484072fa086e64ee4cc9e9c8d807
hauteville86 <karolcyprowski@gmail.com>
2017-01-05 23:35:56 +0100

    Add basic User methods

--- src/main/java/pl/karolcyprowski/simple/srs/dao/UserDAOImpl.java
+++ src/main/java/pl/karolcyprowski/simple/srs/dao/UserDAOImpl.java
@@ -1,5 +1,8 @@
 package pl.karolcyprowski.simple.srs.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import pl.karolcyprowski.simple.srs.entities.User;
@@ -7,10 +10,13 @@ import pl.karolcyprowski.simple.srs.entities.User;
 @Repository
 public class UserDAOImpl implements UserDAO {
 
+	@Autowired
+	private SessionFactory sessionFactory;
+	
 	@Override
 	public void addUser(User user) {
-		// TODO Auto-generated method stub
-		
+		Session currentSession = sessionFactory.getCurrentSession();
+		currentSession.saveOrUpdate(user);		
 	}
 
 }
--- src/main/java/pl/karolcyprowski/simple/srs/entities/User.java
+++ src/main/java/pl/karolcyprowski/simple/srs/entities/User.java
@@ -1,10 +1,71 @@
 package pl.karolcyprowski.simple.srs.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="user")
 public class User {
+	
+	@Column(name="username")
+	private String username;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="authority")
+	private String authority;
+	
+	@Column(name="enabled")
+	private boolean enabled;
+	
+	public User()
+	{
+		
+	}
+
+	public User(String username, String password, String authority, boolean enabled) {
+		this.username = username;
+		this.password = password;
+		this.authority = authority;
+		this.enabled = enabled;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	
+	
+	
 
 }
