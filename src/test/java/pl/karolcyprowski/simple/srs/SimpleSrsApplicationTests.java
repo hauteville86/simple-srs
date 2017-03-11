8f6446cc42b609c50407003edd4604d374fd600c
hauteville86 <karolcyprowski@gmail.com>
2017-03-11 11:14:57 +0100

    Add simple test

--- src/test/java/pl/karolcyprowski/simple/srs/SimpleSrsApplicationTests.java
+++ src/test/java/pl/karolcyprowski/simple/srs/SimpleSrsApplicationTests.java
@@ -1,9 +1,16 @@
 package pl.karolcyprowski.simple.srs;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import pl.karolcyprowski.simple.srs.business.SimpleSrsGlossAlgorithm;
+import pl.karolcyprowski.simple.srs.business.SrsButton;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +18,10 @@ public class SimpleSrsApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		SimpleSrsGlossAlgorithm algorithm = new SimpleSrsGlossAlgorithm();
+		List<SrsButton> buttonList = algorithm.getButtons();
+		int buttonListSize = buttonList.size();
+		assertEquals(buttonListSize, 4);
 	}
 
 }
